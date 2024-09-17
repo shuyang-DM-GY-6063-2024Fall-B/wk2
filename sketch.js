ad3c85d8c969f91a054ce66e835dbdbee2ff40e8
TreeAspen <sy4254@nyu.edu>
2024-09-16 20:05:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(121, 223,33);
+  // line();
+  rect(100-frameCount, 100+frameCount, 200,100);
+  // ellipse(x, y, w, [h])
+  // quad(x1, y1, x2, y2, x3, y3, x4, y4)
+  // fill(v1, [v2], [v3], [a])
+  // stroke(v1, [v2], [v3], [a])
+  // strokeWeight(weight,height,windowHeight,windowWidth,mouseX,mouseY)
 }
 
 function draw() {
